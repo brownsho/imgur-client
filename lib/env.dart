1e564e1b5442945837b53ff831d72da3f7c330e5
Kisho <shokipsh@gmail.com>
2020-12-11 23:11:23 +0100

    [change]: env.dart

--- lib/env.dart
+++ lib/env.dart
@@ -1,6 +1,6 @@
 const api = {
-  'clientID': '9e9995b746179c5',
-  'clientSecret': '35a829f02556801267e27adbc227cb2fd70d93cc',
+  'clientID': 'imgur client id',
+  'clientSecret': 'imgur client secret',
   'responseType': 'token',
-  'state': 'epicture',
+  'state': 'imgur-client',
 };
